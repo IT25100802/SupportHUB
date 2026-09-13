ac166b5f28a61feebd6ce642f32166b08179e1f2
Madhusankha K.A.D <it25102645@my.sliit.lk>
2026-09-13 16:21:16 +0530

    Add ChatbotQueryRequest and ChatbotQueryResponse classes

---
+++ backend/src/main/java/com/customersupport/SupportHUB/knowledgebase/ChatbotQueryResponse.java
@@ -0,0 +1,64 @@
+package com.customersupport.SupportHUB.knowledgebase;
+
+import java.util.List;
+
+public class ChatbotQueryResponse {
+
+    private boolean matched;
+    private String answer;
+    private Double confidenceScore;
+    private String matchedStrategy;
+    private List<FaqArticleDto> relatedFaqs;
+    private boolean suggestTicket;
+
+    public ChatbotQueryResponse() {
+    }
+
+    public boolean isMatched() {
+        return matched;
+    }
+
+    public void setMatched(boolean matched) {
+        this.matched = matched;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+
+    public Double getConfidenceScore() {
+        return confidenceScore;
+    }
+
+    public void setConfidenceScore(Double confidenceScore) {
+        this.confidenceScore = confidenceScore;
+    }
+
+    public String getMatchedStrategy() {
+        return matchedStrategy;
+    }
+
+    public void setMatchedStrategy(String matchedStrategy) {
+        this.matchedStrategy = matchedStrategy;
+    }
+
+    public List<FaqArticleDto> getRelatedFaqs() {
+        return relatedFaqs;
+    }
+
+    public void setRelatedFaqs(List<FaqArticleDto> relatedFaqs) {
+        this.relatedFaqs = relatedFaqs;
+    }
+
+    public boolean isSuggestTicket() {
+        return suggestTicket;
+    }
+
+    public void setSuggestTicket(boolean suggestTicket) {
+        this.suggestTicket = suggestTicket;
+    }
+}
