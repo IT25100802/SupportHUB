4db25066bfe898b6ed74a91802058c2881b1ca66
IT25100802 <IT25100802@my.sliit.lk>
2026-09-25 11:12:00 +0530

    Add CategoryService interface for category management operations

---
+++ backend/src/main/java/com/customersupport/SupportHUB/category/CategoryService.java
@@ -0,0 +1,15 @@
+package com.customersupport.SupportHUB.category;
+
+import java.util.List;
+
+public interface CategoryService {
+    TicketCategoryDto createCategory(CreateCategoryRequest request);
+    TicketCategoryDto getCategoryById(Long id);
+    List<TicketCategoryDto> getAllCategories();
+    List<TicketCategoryDto> getActiveCategories();
+    TicketCategoryDto updateCategory(Long id, CreateCategoryRequest request);
+    TicketCategoryDto toggleCategoryActive(Long id);
+    void deleteCategory(Long id);
+    List<TicketCategoryDto> getSubcategoriesByCategoryId(Long parentId);
+    TicketCategoryDto createSubcategory(Long parentId, CreateCategoryRequest request);
+}
